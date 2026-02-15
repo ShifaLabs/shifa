147e938bd16ead25c187b5f973f7edb278559ec6
Sojib Ahmed <mdaminurislamsojib2006@gmail.com>
2026-02-15 13:42:27 +0600

    [UPDATE]: Update Home page text

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className=" h-screen w-full align-middle items-center text-3xl flex justify-center">
       <h1 className=" text-center font-bold ">
-        Welcome to SHIFA from the developer team!
+        Welcome to SHIFA from the developer team! - Sojib Ahmed
       </h1>
     </div>
   );
