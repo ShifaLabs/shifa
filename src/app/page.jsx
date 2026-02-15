d82668458c5ea813625d003f132f0627bb2707ea
Sojib Ahmed <mdaminurislamsojib2006@gmail.com>
2026-02-15 12:32:05 +0600

    [MODIFIED]: modifiend the home page heading text

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <div className=" h-screen w-full align-middle items-center text-3xl flex justify-center">
-      <h1 className=" text-center font-bold ">Welcome to SHIFA</h1>
+      <h1 className=" text-center font-bold ">
+        Welcome to SHIFA App from the Sojib Ahmed Side
+      </h1>
     </div>
   );
 }
