425d4a7fb8862c807d887158e6f71e920223a8b9
Sojib Ahmed <mdaminurislamsojib2006@gmail.com>
2026-02-17 16:32:14 +0600

    [MODIFIED]: add suppressHydrationWarning to body element in RootLayout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -22,6 +22,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        suppressHydrationWarning
       >
         <main className="mt-20">
           <Container>{children}</Container>
