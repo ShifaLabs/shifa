077fd307b0849b105fe9175b5838d8de509d84f7
Sojib Ahmed <mdaminurislamsojib2006@gmail.com>
2026-02-19 02:19:15 +0600

    feat: implement Google login API, new Navbar component, Home page structure, and a dashboard consultation page.

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -1,5 +1,4 @@
 import CTASection from "@/components/Home/CTASection";
-import { hindSiliguri } from "../layout";
 import BlogSection from "@/components/Home/BlogSection";
 import TestimonialsSection from "@/components/Home/TestimonialsSection";
 import WhyChooseSection from "@/components/Home/WhyChooseSection";
@@ -13,17 +12,6 @@ import FAQ from "@/components/Home/FAQ";
 
 const Home = () => {
   return (
-    // <div className=" h-screen  w-full align-middle items-center text-3xl flex justify-center flex-col">
-    //   <h1 className=" text-center font-bold ">
-    //     Welcome to SHIFA from the developer team!
-    //   </h1>
-    //   <p
-    //     className={`${hindSiliguri.className} text-2xl font-semibold mt-4 text-center`}
-    //   >
-    //     উন্নত ও সহজলভ্য স্বাস্থ্যসেবার জন্য আপনার নির্ভরযোগ্য টেলিমেডিসিন
-    //     প্ল্যাটফর্ম।
-    //   </p>
-    // </div>
     <>
       <Container>
         <ServicesSection />
