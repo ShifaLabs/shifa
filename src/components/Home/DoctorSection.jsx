109e5f8682e95cb1fa1d562ead7fd6596c2d29d9
Sojib Ahmed <mdaminurislamsojib2006@gmail.com>
2026-03-11 19:51:11 +0600

    Refactor code structure for improved readability and maintainability

--- src/components/Home/DoctorSection.jsx
+++ src/components/Home/DoctorSection.jsx
@@ -26,7 +26,7 @@ const DoctorsSection = async () => {
             <div className="p-6 border rounded-xl shadow hover:shadow-2xl transition duration-300">
               <div className="relative h-32 w-32 rounded-full mx-auto mb-4 border overflow-hidden">
                 <Image
-                  src={doctor?.profileImage}
+                  src={doctor?.profileImage || "/male-doctor.png"}
                   alt={doctor.fullName}
                   fill
                   className="object-cover"
