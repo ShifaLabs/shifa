6abf57412023702bf5f9bf60fe72b001bbb60bba
Sojib Ahmed <mdaminurislamsojib2006@gmail,com>
2026-03-15 23:42:08 +0600

    feat: update session and user types to include admin role and doctorId

--- src/app/dashboard/doctor/patients/page.tsx
+++ src/app/dashboard/doctor/patients/page.tsx
@@ -7,22 +7,22 @@ import {
 } from "@/features/patients/patients.doctor.service";
 
 export default async function DoctorPatientsPage() {
-  //   const session = await getServerSession(authOptions);
-
-  //   if (!session) {
-  //     return (
-  //       <div className="min-h-screen flex items-center justify-center">
-  //         <p className="text-lg font-medium">
-  //           Please login to view your patients.
-  //         </p>
-  //       </div>
-  //     );
-  //   }
+  const session = await getServerSession(authOptions);
+  console.log("Doctor patients page session:", session);
+  if (!session) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <p className="text-lg font-medium">
+          Please login to view your patients.
+        </p>
+      </div>
+    );
+  }
 
   //   const activePatients = await getActivePatientsForDoctor(session.user.id);
   //   const pastPatients = await getPastPatientsForDoctor(session.user.id);
 
-  const doctorId = "69b64c20c5c00036c0804379";
+  const doctorId = session.user.doctorId || session.user.id;
   const activePatients = (await getActivePatientsForDoctor(doctorId)).map(
     (p) => ({
       ...p,
@@ -30,7 +30,7 @@ export default async function DoctorPatientsPage() {
       nextAppointment: p.nextAppointment?.toISOString(),
     }),
   );
-
+  console.log("Active patients:", activePatients);
   const pastPatients = (await getPastPatientsForDoctor(doctorId)).map((p) => ({
     ...p,
     _id: p._id.toString(),
