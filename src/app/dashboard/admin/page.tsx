83757f24e7120626f5ca5d5729dd2648d2a88625
Sojib Ahmed <mdaminurislamsojib2006@gmail,com>
2026-03-16 14:06:25 +0600

    [REFACTOR]: remove unused imports from admin page component

--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -1,7 +1,3 @@
-import Ai from "@/features/ai/ai";
-import ShifaChatbot from "@/features/ai/ShifaChatbot";
-import React from "react";
-
 export default function page() {
   return <div>Admin page</div>;
 }
